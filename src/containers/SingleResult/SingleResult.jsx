efbc867ee71291504fa6c0d5f4322766a68dae9c
RohitKaushal7 <rohitkk252550@gmail.com>
2021-02-24 19:48:50 +0530

    ui

--- src/containers/SingleResult/SingleResult.jsx
+++ src/containers/SingleResult/SingleResult.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import Header from "../../components/Header/Header";
 import "./SingleResult.scss";
 
 export default function SingleResult({ history, match }) {
@@ -8,61 +10,66 @@ export default function SingleResult({ history, match }) {
     fetch(`https://nithp.herokuapp.com/api/result/student/${match.params.roll}`)
       .then((res) => res.json())
       .then((res) => {
-        console.log(res);
         setResult(res);
       });
   }, []);
 
   if (!result) {
-    return null;
+    return <div className="loading">loading...</div>;
   }
 
   let semesters = [8, 7, 6, 5, 4, 3, 2, 1]
     .map((sem) => result.result.filter((x) => x.sem == sem))
     .filter((sem) => sem.length);
 
-  console.log(semesters);
   return (
     <div className="fullResult">
-      <div className="stInfo">
-        <div className="stDesc">
-          <div className="stName">{result.name}</div>
-          <div className="stRoll">
-            {result.roll} {result.branch}
+      <header>
+        <div className="stInfo">
+          <div className="stDesc">
+            <div className="stName">{result.name}</div>
+            <div className="stRoll">
+              {result.roll} {result.branch}
+            </div>
+          </div>
+          <div className="stcg">
+            <div className="cp">{result.cgpi}</div>
+            <div className="cp_total">
+              {result.summary[result.summary.length - 1].cgpi_total}
+            </div>
+          </div>
+          <div className="ranks">
+            <div className="rk">
+              <span className="rkt">#_ {result.rank.class.cgpi}</span> class
+            </div>
+            <div className="rk">
+              <span className="rkt">#_ {result.rank.year.cgpi}</span> year
+            </div>
+            <div className="rk">
+              <span className="rkt">#_ {result.rank.college.cgpi}</span> college
+            </div>
           </div>
         </div>
-        <div className="stcg">
-          <div className="cp">{result.cgpi}</div>
-          <div className="cp_total">
-            {result.summary[result.summary.length - 1].cgpi_total}
-          </div>
+        <div className="home">
+          <Link to="/">
+            <Header />
+          </Link>
         </div>
-        <div className="ranks">
-          <div className="rk">
-            <span className="rkt">#_ {result.rank.class.cgpi}</span> class
-          </div>
-          <div className="rk">
-            <span className="rkt">#_ {result.rank.year.cgpi}</span> year
-          </div>
-          <div className="rk">
-            <span className="rkt">#_ {result.rank.college.cgpi}</span> college
-          </div>
-        </div>
-      </div>
+      </header>
       <div className="stSemesters">
-        {semesters.map((sem) => (
-          <div className="sem">
+        {semesters.map((sem, i) => (
+          <div className="sem" key={i}>
             <div className="stsg">
-              <div class="semN">Sem {sem[0].sem}</div>
-              <div class="sg_total">
+              <div className="semN">Sem {sem[0].sem}</div>
+              <div className="sg_total">
                 {" "}
                 +{result.summary[sem[0].sem - 1].sgpi_total}
               </div>
-              <div class="sg">{result.summary[sem[0].sem - 1].sgpi}</div>
+              <div className="sg">{result.summary[sem[0].sem - 1].sgpi}</div>
             </div>
             <div className="subs">
               {sem.map((subj) => (
-                <div className="sub">
+                <div className="sub" key={subj.subject}>
                   <div className="code">{subj.subject}</div>
                   <div className={`grade ${subj.grade}`}>
                     {Number(subj.sub_gp) / Number(subj.sub_point)}
