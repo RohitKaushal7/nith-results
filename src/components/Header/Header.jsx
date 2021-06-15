d6604ada4dcc7cd0ac5d266b56ce9a5049c15d4e
RohitKaushal7 <rohitkk252550@gmail.com>
2021-06-15 17:19:54 +0530

    minor changes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,14 +4,32 @@ import "./Header.scss";
 import icon from "../../assets/hero.png";
 import { useGlobalContext } from "../../context/GlobalContext";
 
+const months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sept",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+
 export default function Header({}) {
   const { version } = useGlobalContext();
   return (
     <div className="header">
       <img src={icon} alt="" className="logo" />
       <div className="main-heading">
-        <b>NITH RESULTS</b>
-        <span className="date">{new Date(version).toDateString()}</span>
+        <h2>NITH RESULTS</h2>
+        <div className="date">
+          {months[new Date(version).getMonth()]}{" "}
+          {new Date(version).getFullYear()}
+        </div>
       </div>
     </div>
   );
